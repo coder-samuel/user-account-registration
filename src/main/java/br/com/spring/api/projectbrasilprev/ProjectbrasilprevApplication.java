bdc59e6faa512bd39baaeaaaee89324145242cd2
coder-samuel <samuelsantos.ss95@gmail.com>
2021-11-29 01:03:05 -0200

    First commit

---
+++ src/main/java/br/com/spring/api/projectbrasilprev/ProjectbrasilprevApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring.api.projectbrasilprev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectbrasilprevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectbrasilprevApplication.class, args);
+	}
+
+}
