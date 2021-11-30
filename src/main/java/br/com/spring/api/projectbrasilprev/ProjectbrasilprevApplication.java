68c2fada08b024a003e2cb86217ab4c1a741e1cd
coder-samuel <samuelsantos.ss95@gmail.com>
2021-11-29 23:19:26 -0200

    Connect to database

--- src/main/java/br/com/spring/api/projectbrasilprev/ProjectbrasilprevApplication.java
+++ src/main/java/br/com/spring/api/projectbrasilprev/ProjectbrasilprevApplication.java
@@ -3,6 +3,7 @@ package br.com.spring.api.projectbrasilprev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProjectbrasilprevApplication {
 
